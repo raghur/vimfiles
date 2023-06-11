354bc07b706eefcdfba337682932f1893c96903b
Raghu Rajagopalan <raghu.rajagopalan@gmail.com>
2023-06-11 12:04:41 +0530

    fix: nvim-cmp commandline mappings

--- plugins/nvim-cmp.lua
+++ plugins/nvim-cmp.lua
@@ -82,7 +82,8 @@ cmp.setup({
 })
 
 -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline('/', {
+cmp.setup.cmdline({'/', '?'}, {
+  mapping = cmp.mapping.preset.cmdline();
   sources = {
     { name = 'buffer' }
   }
@@ -90,6 +91,7 @@ cmp.setup.cmdline('/', {
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline();
   sources = cmp.config.sources({
     { name = 'path' }
   }, {
