b3d20395e87223558eac9aa034f879075fec75dc
Raghu Rajagopalan <raghu.rajagopalan@gmail.com>
2023-08-14 17:24:02 +0530

    fix: sessions - create dir if not exists

--- lua/raghu/vim-session.lua
+++ lua/raghu/vim-session.lua
@@ -1,6 +1,8 @@
 local instance = {}
 instance.config = function()
   vim.g.session_directory = vim.fn.stdpath("state") .. "/.vimbackups/.sessions"
+  local createDir = vim.fn['utils#createIfNotExists']
+  createDir(vim.g.session_directory)
   print(vim.g.session_directory)
   vim.g.session_command_aliases = 1
   vim.g.session_autosave = "yes"
