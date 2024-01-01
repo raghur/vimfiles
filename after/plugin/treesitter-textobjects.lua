4fd56159b487e35635ef7cec97b64c507f511eef
Raghu Rajagopalan <raghu.rajagopalan@gmail.com>
2024-01-01 08:03:10 +0530

    treesitter textobjects mappings

--- after/plugin/treesitter-textobjects.lua
+++ after/plugin/treesitter-textobjects.lua
@@ -8,14 +8,21 @@ require'nvim-treesitter.configs'.setup {
 
       keymaps = {
         -- You can use the capture groups defined in textobjects.scm
-        ["af"] = {query = "@function.outer", desc = "Function outer"},
-        ["if"] = { query = "@function.inner", desc = "Function inner" },
+        ["ip"] = {query = "@parameter.inner", desc = "parameter inner"},
+        ["ap"] = {query = "@parameter.outer", desc = "parameter outer"},
+        ["af"] = {query = "@function.outer", desc = "function outer"},
+        ["if"] = { query = "@function.inner", desc = "function inner" },
         ["ac"] = {query = "@conditional.outer", desc = "conditional outer"},
         -- You can optionally set descriptions to the mappings (used in the desc parameter of
         -- nvim_buf_set_keymap) which plugins like which-key display
         ["ic"] = { query = "@conditional.inner", desc = "conditional inner" },
         -- You can also use captures from other query groups like `locals.scm`
-        ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+        ["as"] = { query = "@scope", query_group = "locals", desc = "scope" },
+      },
+      selection_modes = {
+        ['@parameter.outer'] = 'v', -- charwise
+        ['@function.outer'] = 'V', -- linewise
+        ['@class.outer'] = 'V', -- blockwise
       },
       -- You can choose the select mode (default is charwise 'v')
       --
@@ -44,8 +51,10 @@ require'nvim-treesitter.configs'.setup {
       enable = true,
       set_jumps = true, -- whether to set jumps in the jumplist
       goto_next_start = {
-        ["<leader>j"] = {query = "@function.outer", "next function next"},
-        ["<leader>cs"] = { query = "@class.outer", desc = "Next class start" },
+        ["]m"] = {query = "@function.outer", desc = "next function"},
+        ["]c"] = {query = "@class.outer", desc = "next class"},
+        ["]s"] = {query = "@scope", query_group = "locals", desc = "next scope"},
+        ["]f"] = {query = "@fold", query_group = "folds", desc = "next fold"},
         --
         -- You can use regex matching (i.e. lua pattern) and/or pass a list in a "query" key to group multiple queires.
         -- ["<leader>sn"] = "@loop.*",
@@ -53,30 +62,35 @@ require'nvim-treesitter.configs'.setup {
         --
         -- You can pass a query group to use query from `queries/<lang>/<query_group>.scm file in your runtime path.
         -- Below example nvim-treesitter's `locals.scm` and `folds.scm`. They also provide highlights.scm and indent.scm.
-        ["<leader>sn"] = { query = "@scope", query_group = "locals", desc = "Next scope" },
         -- ["<leader>z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
       },
-      goto_next_end = {
-        ["<leader>me"] = "@function.outer",
-        ["<leader>ee"] = "@class.outer",
-      },
       goto_previous_start = {
-        ["<leader>k"] = "@function.outer",
-        ["<leader>cp"] = "@class.outer",
-      },
-      goto_previous_end = {
-        ["<leader>mM"] = "@function.outer",
-        ["[]"] = "@class.outer",
+        ["[m"] = {query = "@function.outer", desc = "prev function"},
+        ["[c"] = {query = "@class.outer", desc = "prev class"},
+        ["[s"] = {query = "@scope", query_group = "locals", desc = "prev scope"},
+        ["[f"] = {query = "@fold", query_group = "folds", desc = "prev fold"},
+      --   ["<leader>k"] = "@function.outer",
+      --   ["<leader>cp"] = "@class.outer",
       },
       -- Below will go to either the start or the end, whichever is closer.
       -- Use if you want more granular movements
       -- Make it even more gradual by adding multiple queries and regex.
-      goto_next = {
-        ["]d"] = "@conditional.outer",
-      },
-      goto_previous = {
-        ["[d"] = "@conditional.outer",
-      }
+      -- goto_next = {
+      --   ["]m"] = {query = "@function.outer", desc = "next function"},
+      --   ["]c"] = {query = "@class.outer", desc = "next class"},
+      --   ["]s"] = {query = {"@conditional.outer", "@loop.outer"}, desc = "next block"},
+      -- },
+      -- goto_previous = {
+      --   ["[m"] = {query = "@function.outer", desc = "next function"},
+      --   ["[c"] = {query = "@class.outer", desc = "next class"},
+      --   ["[s"] = {query = {"@conditional.outer", "@loop.outer"}, desc = "next block"},
+      -- }
     },
   },
 }
+local ts_repeat_move = require "nvim-treesitter.textobjects.repeatable_move"
+
+-- Repeat movement with ; and ,
+-- ensure ; goes forward and , goes backward regardless of the last direction
+vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move_next)
+vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_previous)
