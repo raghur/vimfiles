2b7b2ecb92ae85ee9c83bc9bfa7a1995e5bc2795
Raghu Rajagopalan <raghu.rajagopalan@gmail.com>
2024-01-01 15:10:59 +0530

    null-ls: required servers

--- lua/raghu/mason-null-ls-nvim.lua
+++ lua/raghu/mason-null-ls-nvim.lua
@@ -6,16 +6,19 @@ M.config = function()
 
   require("mason-null-ls").setup({
     ensure_installed = {
+      'stylua',
+      'jq',
+      'beautysh',
+      'prettierd',
+      'lua-language-server',
+      'typescript-language-server',
+      'vim-language-server',
       -- Opt to list sources here, when available in mason.
     },
     automatic_installation = false,
     handlers = {},
   })
-  require("null-ls").setup({
-    sources = {
-      -- Anything not supported by mason.
-    }
-  })
+  require("null-ls").setup()
   Info("sourced", vim.fn.expand("<sfile>"))
 end
 return M
