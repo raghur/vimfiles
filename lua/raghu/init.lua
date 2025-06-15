a377595e7194ce4b2934c5b744df94b6bc7825cb
Raghu Rajagopalan <raghu.rajagopalan@gmail.com>
2025-06-15 19:36:39 +0530

    chore: clean up

--- lua/raghu/init.lua
+++ lua/raghu/init.lua
@@ -1,18 +1,18 @@
 
 local utils = require('raghu.utils')
 local instance = {}
-
+local vi = vim.inspect
 local default = require('lazy.core.loader')
 instance.configurePlugin = function (plugin, opts)
   -- preserve default option setup
   local main = default.get_main(plugin)
   if main and next(opts) then
-    Info("running default setup(opts)", vim.inspect(main), plugin.name, vim.inspect(opts))
+    utils.info("running default setup(opts)", vi(main), plugin.name, vi(opts))
     require(main).setup(opts)
   end
   local config = require('raghu.'.. string.gsub(plugin.name, '%.', '-'))
   config.config(opts)
   utils.info('configured', plugin.name)
-  utils.dbg(vim.inspect(plugin), vim.inspect(opts))
+  utils.dbg(vi(plugin), vi(opts))
 end
 return instance
