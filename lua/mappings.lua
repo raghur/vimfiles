f604043f17b197a65d1def50f4fa0bc4866167e7
Raghu Rajagopalan <raghu.rajagopalan@gmail.com>
2025-06-26 23:35:34 +0530

    chore: formatting

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -66,73 +66,78 @@ local font = require("raghu.font")
 local snacks = require("snacks")
 
 local mappings = {
-    { "<leader>f", group = "+Files"},
-    { "<leader>ff",snacks.picker.files, desc = "Find relative"},
-    { "<leader>fp",snacks.picker.zoxide, desc = "Find files at"},
-    { "<leader>fe", ":edit <C-R>=fnamemodify(@%, ':p:h')<CR>/", desc = "edit file" },
-    { "<leader>r", snacks.picker.recent, desc = "Find recent" },
-    { "<leader>b", snacks.picker.buffers, desc = "Buffers" },
-    { "<leader>/", snacks.picker.grep, desc = "Grep" },
-    { "<leader><space>",function() snacks.picker.smart({ hidden=true
-    }) end, desc = "Find relative"},
-    { "<leader>e", ":vsp|Neorg index<CR>", desc = "Scratchpad Notes" },
-    { "<leader>sk", snacks.picker.keymaps, desc = "Keymaps" },
-    { "<leader>sc", snacks.picker.commands, desc = "Commands" },
+  { "<leader>f",       group = "+Files" },
+  { "<leader>ff",      snacks.picker.files,                                   desc = "Find relative" },
+  { "<leader>fp",      snacks.picker.zoxide,                                  desc = "Find files at" },
+  { "<leader>fe",      ":edit <C-R>=fnamemodify(@%, ':p:h')<CR>/",            desc = "edit file" },
+  { "<leader>r",       snacks.picker.recent,                                  desc = "Find recent" },
+  { "<leader>b",       snacks.picker.buffers,                                 desc = "Buffers" },
+  { "<leader>/",       snacks.picker.grep,                                    desc = "Grep" },
+  { "<leader><space>", function() snacks.picker.smart({ hidden = true }) end, desc = "Find relative" },
+  { "<leader>e",       ":vsp|Neorg index<CR>",                                desc = "Scratchpad Notes" },
+  { "<leader>sk",      snacks.picker.keymaps,                                 desc = "Keymaps" },
+  { "<leader>sc",      snacks.picker.commands,                                desc = "Commands" },
 
 
-    -- terminal apps
-    { "<leader>t", group = "Terminal apps"},
-    { "<leader>tg", function() snacks.lazygit.open() end, desc = "LazyGit" },
-    { "<leader>tt", function() snacks.terminal.toggle(nil, {
+  -- terminal apps
+  { "<leader>t",       group = "Terminal apps" },
+  { "<leader>tg",      function() snacks.lazygit.open() end,                  desc = "LazyGit" },
+  {
+    "<leader>tt",
+    function()
+      snacks.terminal.toggle(nil, {
         win = {
-            style = "terminal",
-            position = "right",
+          style = "terminal",
+          position = "right",
         }
-    }) end, desc = "Open terminal" },
+      })
+    end,
+    desc = "Open terminal"
+  },
 
-    -- git
-    { "<leader>g", group = "Git"},
-    { "<leader>gg", snacks.picker.git_files, desc = "Git files" },
-    { "<leader>gb", function() snacks.picker.git_branches() end, desc = "Git Branches" },
-    { "<leader>gl", function() snacks.picker.git_log() end, desc = "Git Log" },
-    { "<leader>gL", function() snacks.picker.git_log_line() end, desc = "Git Log Line" },
-    { "<leader>gs", function() snacks.picker.git_status() end, desc = "Git Status" },
-    { "<leader>gS", function() snacks.picker.git_stash() end, desc = "Git Stash" },
-    { "<leader>gd", function() snacks.picker.git_diff() end, desc = "Git Diff (Hunks)" },
-    { "<leader>gf", function() snacks.picker.git_log_file() end, desc = "Git Log File" },
-    { "<leader>g/", function() snacks.picker.git_grep() end, desc = "Git Log File" },
+  -- git
+  { "<leader>g",  group = "Git" },
+  { "<leader>gg", snacks.picker.git_files,                     desc = "Git files" },
+  { "<leader>gb", function() snacks.picker.git_branches() end, desc = "Git Branches" },
+  { "<leader>gl", function() snacks.picker.git_log() end,      desc = "Git Log" },
+  { "<leader>gL", function() snacks.picker.git_log_line() end, desc = "Git Log Line" },
+  { "<leader>gs", function() snacks.picker.git_status() end,   desc = "Git Status" },
+  { "<leader>gS", function() snacks.picker.git_stash() end,    desc = "Git Stash" },
+  { "<leader>gd", function() snacks.picker.git_diff() end,     desc = "Git Diff (Hunks)" },
+  { "<leader>gf", function() snacks.picker.git_log_file() end, desc = "Git Log File" },
+  { "<leader>g/", function() snacks.picker.git_grep() end,     desc = "Git Log File" },
 }
 wk.add(mappings)
 
 mappings = {
-  { "<leader>c", function() snacks.picker.colorschemes() end, desc = "Colors" },
-  { "<leader>:", snacks.picker.commands, desc = "Commands" },
-  { "<leader>1", "<cmd>on<cr>", desc = "Close others" },
-  { "<leader>a", ":b#<cr>", desc = "Last file" },
-  { "<leader>d", ":bd!<cr>", desc = "Close buffer" },
-  { "<leader>h", ":noh<cr><c-l>", desc = "no highlight" },
-  { "<leader>q", ":qall<cr>", desc = "Quit" },
-  { "<leader>w", ":w<cr>", desc = "Write" },
-  { "<leader>z", ":call utils#toggleZoom()<cr>", desc = "Zoom" },
-  {'zR', function() require('ufo').openAllFolds() end, desc = "Open all folds"},
-  {'zM', function() require('ufo').closeAllFolds() end, desc = "Close all folds"}
+  { "<leader>c", function() snacks.picker.colorschemes() end,   desc = "Colors" },
+  { "<leader>:", snacks.picker.commands,                        desc = "Commands" },
+  { "<leader>1", "<cmd>on<cr>",                                 desc = "Close others" },
+  { "<leader>a", ":b#<cr>",                                     desc = "Last file" },
+  { "<leader>d", ":bd!<cr>",                                    desc = "Close buffer" },
+  { "<leader>h", ":noh<cr><c-l>",                               desc = "no highlight" },
+  { "<leader>q", ":qall<cr>",                                   desc = "Quit" },
+  { "<leader>w", ":w<cr>",                                      desc = "Write" },
+  { "<leader>z", ":call utils#toggleZoom()<cr>",                desc = "Zoom" },
+  { 'zR',        function() require('ufo').openAllFolds() end,  desc = "Open all folds" },
+  { 'zM',        function() require('ufo').closeAllFolds() end, desc = "Close all folds" }
 }
 wk.add(mappings)
 
 mappings = {
-    { "<leader>i", group = "+Config" },
-    { "<leader>im", function() utils.editConfig("mappings.lua") end, desc = "Mappings" },
-    { "<leader>ir", utils.reload, desc = "Reload settings and mappings" },
-    { "<leader>ii", "<Cmd>ed $MYVIMRC<cr>", desc = "Edit init.lua" },
-    { "<leader>is", function() utils.editConfig("settings.lua") end, desc = "Settings" },
-    { "<leader>ip", function() utils.editConfig("plugins.lua") end, desc = "Plugins" },
-    { "<leader>ic", function() utils.editConfigFolder("raghu") end, desc = "Plugin Config Folder" },
-    { "<leader>i,", "<cmd>source %<cr>", desc = "Source file" },
-    { "<leader>il", group = "+Logging" },
-    { "<leader>ild", function() utils.loglvl("DBG") end, desc = "Log - Debug" },
-    { "<leader>ili", function() utils.loglvl("INFO") end, desc = "Log - INFO" },
-    { "<leader>ilx", function() utils.loglvl("DISABLE") end, desc = "Log - disable" },
-    { "<leader>in",  snacks.notifier.show_history , desc = "Notification history" },
+  { "<leader>i",   group = "+Config" },
+  { "<leader>im",  function() utils.editConfig("mappings.lua") end, desc = "Mappings" },
+  { "<leader>ir",  utils.reload,                                    desc = "Reload settings and mappings" },
+  { "<leader>ii",  "<Cmd>ed $MYVIMRC<cr>",                          desc = "Edit init.lua" },
+  { "<leader>is",  function() utils.editConfig("settings.lua") end, desc = "Settings" },
+  { "<leader>ip",  function() utils.editConfig("plugins.lua") end,  desc = "Plugins" },
+  { "<leader>ic",  function() utils.editConfigFolder("raghu") end,  desc = "Plugin Config Folder" },
+  { "<leader>i,",  "<cmd>source %<cr>",                             desc = "Source file" },
+  { "<leader>il",  group = "+Logging" },
+  { "<leader>ild", function() utils.loglvl("DBG") end,              desc = "Log - Debug" },
+  { "<leader>ili", function() utils.loglvl("INFO") end,             desc = "Log - INFO" },
+  { "<leader>ilx", function() utils.loglvl("DISABLE") end,          desc = "Log - disable" },
+  { "<leader>in",  snacks.notifier.show_history,                    desc = "Notification history" },
 }
 wk.add(mappings)
 
@@ -144,52 +149,52 @@ local sourceRange = function()
     vim.cmd(cmd)
 end
 mappings = {
-    { "<leader>i", group = "+Config", mode = "v" },
-    { "<leader>i,", sourceRange, desc = "Source lines", mode = "v" },
+  { "<leader>i",  group = "+Config", mode = "v" },
+  { "<leader>i,", sourceRange,       desc = "Source lines", mode = "v" },
 }
 wk.add(mappings)
 
 wk.add({
-    { "S", ":<C-U>lua MiniSurround.add('visual')<cr>", desc = "Surround", mode = "xn" },
+  { "S", ":<C-U>lua MiniSurround.add('visual')<cr>", desc = "Surround", mode = "xn" },
 })
 
 mappings = {
-    { "<S-insert>", '"0p', desc = "paste", mode = "v"},
-    { "p", '"0p', desc = "paste", mode = "v"},
-    { "P", '"0P', desc = "paste before", mode = "v"},
-    { "<leader>p", '"0p', desc = 'Paste "0' },
-    { "<leader>P", '"0P', desc = 'Paste "0 before' },
+  { "<S-insert>", '"0p', desc = "paste",          mode = "v" },
+  { "p",          '"0p', desc = "paste",          mode = "v" },
+  { "P",          '"0P', desc = "paste before",   mode = "v" },
+  { "<leader>p",  '"0p', desc = 'Paste "0' },
+  { "<leader>P",  '"0P', desc = 'Paste "0 before' },
 }
 wk.add(mappings)
 mappings = {
-    { "<F1>", snacks.explorer.reveal, desc = "Reveal in file explorer" },
-    { "<leader><F1>", snacks.explorer.open, desc = "Toggle file explorer" },
-    { "<F3>", ":redir @a<CR>:g//<CR>:redir END<CR>:new<CR>:put! a<CR>", desc = "Copy last search to buffer" },
-    { "<F9>", "<Cmd>YankyRingHistory<cr>", desc = "Yanky" },
-    { "<M-=>", function() font.adjust(1) end, desc = "Increase Font" },
-    { "<M-->", function() font.adjust(-1) end, desc = "Decrease Font" },
-    { "<M-[>", function() font.cycleFont(-1) end, desc = "Previous Font" },
-    { "<M-]>", function() font.cycleFont(1) end, desc = "Next Font" },
+  { "<F1>",         snacks.explorer.reveal,                                   desc = "Reveal in file explorer" },
+  { "<leader><F1>", snacks.explorer.open,                                     desc = "Toggle file explorer" },
+  { "<F3>",         ":redir @a<CR>:g//<CR>:redir END<CR>:new<CR>:put! a<CR>", desc = "Copy last search to buffer" },
+  { "<F9>",         "<Cmd>YankyRingHistory<cr>",                              desc = "Yanky" },
+  { "<M-=>",        function() font.adjust(1) end,                            desc = "Increase Font" },
+  { "<M-->",        function() font.adjust(-1) end,                           desc = "Decrease Font" },
+  { "<M-[>",        function() font.cycleFont(-1) end,                        desc = "Previous Font" },
+  { "<M-]>",        function() font.cycleFont(1) end,                         desc = "Next Font" },
 }
 wk.add(mappings)
 
 mappings = {
-    { "g",  group = "LSP nav" },
-    { "g.", "<cmd>Lspsaga code_action<cr>",          desc = "code actions" },
-    { "gq", vim.lsp.buf.format,                      desc = "format" },
-    { "g[", "<cmd>Lspsaga diagnostic_jump_prev<cr>", desc = "prev problem" },
-    { "g]", "<cmd>Lspsaga diagnostic_jump_next<cr>", desc = "next problem" },
-    { "gc", "<cmd>Lspsaga rename<cr>",               desc = "rename" },
-    { "gd", snacks.picker.lsp_definitions,           desc = "definitions" },
-    -- { "gk", function() require("refactoring").select_refactor() end, desc = "refactor", mode = { "x", "n" } },
-    { "gk", vim.diagnostic.open_float,               desc = "show line diagnostic" },
-    { "gl", "<cmd>Lspsaga finder<cr>",               desc = "lsp finder" },
-    { "go", "<cmd>Lspsaga outline<cr>",              desc = "diagnostics" },
-    { "gp", snacks.picker.diagnostics,               desc = "diagnostics" },
-    { "gP", snacks.picker.pickers,        desc = "Select picker" },
-    { "gr", snacks.picker.lsp_references,            desc = "references" },
-    { "gs", snacks.picker.lsp_symbols,               desc = "document symbols" },
-    { "gt", snacks.picker.lsp_workspace_symbols,     desc = "workspace symbols" },
+  { "g",  group = "LSP nav" },
+  { "g.", "<cmd>Lspsaga code_action<cr>",          desc = "code actions" },
+  { "gq", vim.lsp.buf.format,                      desc = "format" },
+  { "g[", "<cmd>Lspsaga diagnostic_jump_prev<cr>", desc = "prev problem" },
+  { "g]", "<cmd>Lspsaga diagnostic_jump_next<cr>", desc = "next problem" },
+  { "gc", "<cmd>Lspsaga rename<cr>",               desc = "rename" },
+  { "gd", snacks.picker.lsp_definitions,           desc = "definitions" },
+  -- { "gk", function() require("refactoring").select_refactor() end, desc = "refactor", mode = { "x", "n" } },
+  { "gk", vim.diagnostic.open_float,               desc = "show line diagnostic" },
+  { "gl", "<cmd>Lspsaga finder<cr>",               desc = "lsp finder" },
+  { "go", "<cmd>Lspsaga outline<cr>",              desc = "diagnostics" },
+  { "gp", snacks.picker.diagnostics,               desc = "diagnostics" },
+  { "gP", snacks.picker.pickers,                   desc = "Select picker" },
+  { "gr", snacks.picker.lsp_references,            desc = "references" },
+  { "gs", snacks.picker.lsp_symbols,               desc = "document symbols" },
+  { "gt", snacks.picker.lsp_workspace_symbols,     desc = "workspace symbols" },
 }
 wk.add(mappings)
 -- keymap("n", "<F2>", "<cmd>Lspsaga rename<CR>", { silent = true })
