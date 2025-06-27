7c4c6e4802b15122e2a836c62f01089f1f162cea
Raghu Rajagopalan <raghu.rajagopalan@gmail.com>
2025-06-27 16:02:51 +0530

    visual snippet

--- lua/raghu/nvim-cmp.lua
+++ lua/raghu/nvim-cmp.lua
@@ -13,6 +13,11 @@ M.config = function()
   -- Setup nvim-cmp.
   local cmp = require 'cmp'
   local luasnip = require('luasnip')
+
+  luasnip.config.set_config({ -- Setting LuaSnip config
+    -- Use <Tab> (or some other key if you prefer) to trigger visual selection
+    store_selection_keys = "<C-t>",
+  })
   local lspkind = require('lspkind')
   cmp.setup({
     matching = {
